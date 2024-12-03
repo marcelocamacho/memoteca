aa742fd21f00cd310a16a1fdbb5872d48367d800
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-12-03 15:28:13 -0300

    ng g c componentes/pensamentos/criar-pensamento

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  standalone: false,
+  
+  templateUrl: './criar-pensamento.component.html',
+  styleUrl: './criar-pensamento.component.css'
+})
+export class CriarPensamentoComponent {
+
+}
