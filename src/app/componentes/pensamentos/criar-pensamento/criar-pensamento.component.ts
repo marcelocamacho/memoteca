68e4a4feaadbd8fac3fabac850cc6a5b1531bf91
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-12-03 16:29:44 -0300

    exemplo de property binding e event binding

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,10 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-criar-pensamento',
   standalone: false,
-  
+
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
 export class CriarPensamentoComponent {
+  pensamento={
+    id: '1',
+    conteudo:'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
 
+  criarPensamento(){
+    alert("pensamento criado")
+  }
 }
