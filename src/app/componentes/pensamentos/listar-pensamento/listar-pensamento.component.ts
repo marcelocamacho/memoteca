9fc752ffc6431b3d9f01fb9c2ac1143fec5ca5eb
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-12-05 13:17:08 -0300

    Renderização dos pensamentos no mural. Utilização das diretivas ngIF, ngFor e ngClass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,10 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   standalone: false,
-  
+
   templateUrl: './listar-pensamento.component.html',
   styleUrl: './listar-pensamento.component.css'
 })
 export class ListarPensamentoComponent {
+  listaPensamentos = [
+    {
+      conteudo: 'Comunicação entre componentes',
+      autoria: 'Angular',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Vou receber os dados pela diretiva @input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    }
+  ];
 
 }
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
   standalone: false,
-  
+
   templateUrl: './pensamento.component.html',
   styleUrl: './pensamento.component.css'
 })
 export class PensamentoComponent {
 
+  @Input() pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Nay',
+    modelo: 'modelo3'
+  }
+
+
+  larguraPensamento():string {
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
+  }
 }
