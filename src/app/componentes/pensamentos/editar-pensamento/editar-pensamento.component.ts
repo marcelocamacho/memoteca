a78620ccbd0082c49dc2c105d22fb82817828ffe
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-12-05 16:43:37 -0300

    ng g c componentes/pensamentos/editar-pensamento

---
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-pensamento',
+  standalone: false,
+  
+  templateUrl: './editar-pensamento.component.html',
+  styleUrl: './editar-pensamento.component.css'
+})
+export class EditarPensamentoComponent {
+
+}
