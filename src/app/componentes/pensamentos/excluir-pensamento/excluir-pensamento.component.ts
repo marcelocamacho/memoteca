a5277f9db4641d9ea0bc8fe9f8346240eb39166c
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-12-05 15:54:09 -0300

    ng g c componentes/pensamentos/excluir-pensamento

---
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  standalone: false,
+  
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrl: './excluir-pensamento.component.css'
+})
+export class ExcluirPensamentoComponent {
+
+}
