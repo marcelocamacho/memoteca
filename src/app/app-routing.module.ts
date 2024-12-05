6a2d443291ccc196dd4c8f787b7d6bf2a67f73ca
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-12-05 18:03:03 -0300

    implementa edição de pensamentos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   {path: '',pathMatch: 'full', redirectTo: 'listarPensamento'},
   {path: 'criarPensamento',component: CriarPensamentoComponent},
   {path: 'listarPensamento',component: ListarPensamentoComponent},
-  {path: 'pensamentos/exluirPensamento/:id',component: ExcluirPensamentoComponent}
+  {path: 'pensamentos/exluirPensamento/:id',component: ExcluirPensamentoComponent},
+  {path: 'pensamentos/editarPensamento/:id',component: EditarPensamentoComponent}
 ];
 
 @NgModule({
--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento/pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-pensamento',
   standalone: false,
-  
+
   templateUrl: './editar-pensamento.component.html',
   styleUrl: './editar-pensamento.component.css'
 })
 export class EditarPensamentoComponent {
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ){
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if(id){
+      this.service.buscarPorId(id).subscribe(
+        (pensamento) => {
+          this.pensamento = pensamento
+        }
+      )
+    }
 
+  }
+
+  editarPensamento(){
+    this.service.editar(this.pensamento).subscribe(
+      ()=>{
+        this.router.navigate(['/'])
+      }
+    )
+  }
+
+  voltarParaHome(){
+    this.router.navigate(['/']);
+}
 }
