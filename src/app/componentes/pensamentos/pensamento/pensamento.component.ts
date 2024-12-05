7df12f38db033492e86327e36bbdec90af22a055
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-12-05 09:20:50 -0300

    ng g c componentes/pensamentos/pensamento

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  standalone: false,
+  
+  templateUrl: './pensamento.component.html',
+  styleUrl: './pensamento.component.css'
+})
+export class PensamentoComponent {
+
+}
